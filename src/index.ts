0bdd75f1a7dc49a5105944383ccb2e521e65b476
RamKishore <mramki1803@gmail.com>
2024-08-28 18:53:54 +0530

    requirements

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,16 @@ app.get('/', (req, res) => {
 app.listen(port, ()=> {
     console.log(`Server listening on port ${port}`);
 })
+
+// get mess menu
+// edit menu
+
+// students
+//req: email res: timetable
+
+// teachers
+// req: email res: timetable
+// edit timetable
+// request other faculty for change/swap (generate request id with info, send notification)
+// view requests (requests made and requests to them)
+// accept / reject request (execute change/swap, send notification)
